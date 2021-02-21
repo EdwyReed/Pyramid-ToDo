3eb1b8c9868c554c415385afd9a872746bf13eec
EdwyReed <wadelyst@gmail.com>
2021-02-21 07:01:28 +0200

    Fixing broken css

--- PyramidToDo/views/default.py
+++ PyramidToDo/views/default.py
@@ -3,4 +3,8 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
 def my_view(request):
-    return {'project': 'Pyramid ToDo'}
+    list = ['task1', 'task2']
+    return {
+        'project': 'Pyramid ToDo',
+        'tasks': list,
+    }
