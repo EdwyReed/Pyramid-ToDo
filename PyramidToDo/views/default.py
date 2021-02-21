a09a129fcd5f524f58c5c61a91a9cd00c1f478f7
EdwyReed <wadelyst@gmail.com>
2021-02-21 08:01:59 +0200

    finished with html+css

--- PyramidToDo/views/default.py
+++ PyramidToDo/views/default.py
@@ -3,7 +3,7 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
 def my_view(request):
-    list = ['task1', 'task2']
+    list = ['Помыть машину', 'Убрать за столом', 'Выучить собаку', 'Покормить уроки']
     return {
         'project': 'Pyramid ToDo',
         'tasks': list,
